a1bcf83481b8e3bb25bb6d9472c55e13105d211d
Mahmud-Araf <almahmudaraf@gmail.com>
2023-10-15 07:25:14 +0600

    bug fixed

--- repo/review_repo.py
+++ repo/review_repo.py
@@ -1,12 +1,16 @@
 import models,database
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
-from sqlalchemy import func
+from sqlalchemy.sql import func
 
 def findAvgRating(doctorId,db:Session=Depends(database.get_db)):
 
-    average_star_count = db.query(func.avg(models.Review.starCount)).label('average') \
-    .filter(models.Review.subject_id == doctorId)
+    average_star_count = db.query(func.avg(models.Review.starCount).label("average_star_count")) \
+    .filter(models.Review.subject_id == doctorId) \
+    .scalar()
 
+    if average_star_count is None:
+        raise HTTPException(status_code=404, detail="Doctor not found")
+    
     return average_star_count
 
