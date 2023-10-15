3f581ea67733a8ef95a12d8b8a5fe31807bf0762
Mahmud-Araf <almahmudaraf@gmail.com>
2023-10-15 07:47:25 +0600

    bug fixed

--- repo/review_repo.py
+++ repo/review_repo.py
@@ -8,10 +8,6 @@ def findAvgRating(doctorId,db:Session=Depends(database.get_db)):
     average_star_count = db.query(func.avg(models.Review.starCount).label("average_star_count")) \
     .filter(models.Review.subject_id == doctorId) \
     .scalar()
-
-    if average_star_count is None:
-        print("Y")
-        raise HTTPException(status_code=404, detail="Doctor not found")
     
     return average_star_count
 
